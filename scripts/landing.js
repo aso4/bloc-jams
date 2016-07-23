335982eb371d6cb7dc6aa6c8d89fdf46035b5d15
aso4 <albertyso@gmail.com>
2016-07-23 14:21:46 -0400

    Assignment 8 resubmit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,16 @@
 var animatePoints = function () {
-    //"use strict";
+    "use strict";
     var i, points = document.getElementsByClassName('point'),
-        revealPoint = function () {
-            for (i = 0; i < points.length; i++) {
-
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                points[i].style.transitionDelay = ".5s";
-                points[i].style.backgroundColor = "rgba(255, 255, 255, .1)";
-                points[i].style.borderRadius = "5%";
-            }
+        revealPoint = function (point) {
+            point.style.opacity = 1;
+            point.style.transform = "scaleX(1) translateY(0)";
+            point.style.msTransform = "scaleX(1) translateY(0)";
+            point.style.WebkitTransform = "scaleX(1) translateY(0)";
+            point.style.transitionDelay = ".5s";
+            //points.style.backgroundColor = "rgba(255, 255, 255, .1)";
+            //points.style.borderRadius = "5%";
         };
-    revealPoint(points);
+    for (i = 0; i < points.length; i++) {
+        revealPoint(points[i]);
+    }
 };
